1470c8c4b6c7e35e07293313761856c210a977eb
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-21 17:50:29 -0300

    style: Improved the appearance of About Section on mobile devices using media queries.

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import Button from "../Button";
 import { WhatsAppIcon, PinIcon, MailIcon } from "../Icons";
-import man from "../../images/man.jpg";
+import doctor from "../../images/doctor.jpg";
 
 const StyledSection = styled.section`
-  background: none;
+  background: #fffaf1;
 `;
 
 const Container = styled.div`
   display: flex;
   align-items: center;
+  flex-direction: row-reverse;
   justify-content: space-around;
   flex-wrap: wrap;
   max-width: 1120px;
   margin: 0 auto;
+  gap: 40px;
+
+  @media (max-width: 991px) {
+    flex-direction: column;
+  }
 `;
 
 const Image = styled.img`
@@ -23,17 +29,18 @@ const Image = styled.img`
   background: rgba(0, 133, 111, 0.15);
   border-radius: 20px;
   margin: 160px 0;
+  max-width: 480px;
 
   @media (max-width: 991px) {
     max-width: 100%;
-    margin-top: 80px;
+    margin-top: 0px;
     margin-bottom: 100px;
   }
 `;
 
 const Content = styled.div`
   flex: 1 1 42rem;
-  max-width: 350px;
+  max-width: 570px;
 
   @media (max-width: 991px) {
     max-width: 100%;
@@ -43,21 +50,31 @@ const Content = styled.div`
 const Title = styled.h1`
   font-style: normal;
   font-weight: 700;
+  font-size: 14px;
+  line-height: 150%;
+  letter-spacing: 0.08em;
+  color: #00856f;
+
+  @media (max-width: 991px) {
+    margin-top: 100px;
+  }
+`;
+
+const Subtitle = styled.h2`
+  max-width: 430px;
+  font-style: normal;
+  font-weight: 700;
   font-size: 40px;
   line-height: 52px;
   color: #212529;
-  margin-bottom: 40px;
 `;
 
-const Address = styled.p`
-  display: flex;
-  align-items: center;
+const Description = styled.p`
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
   line-height: 150%;
   color: #495057;
-  margin-bottom: 16px;
 `;
 
 const About = () => {
@@ -65,23 +82,23 @@ const About = () => {
     <StyledSection>
       <Container>
         <Content>
-          <Title>Entre em contato com a gente!</Title>
-          <Address>
-            <PinIcon />
-            R. Amauri Souza, 346
-          </Address>
-          <Address>
-            <MailIcon />
-            contato@doctorcare.com
-          </Address>
-          <Button>
-            <WhatsAppIcon />
-            Agende sua consulta
-          </Button>
+          <Title>SOBRE NÓS</Title>
+          <Subtitle>Entenda quem somos e por que existimos</Subtitle>
+          <Description>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim. Amet mollit non deserunt
+            ullamco est sit aliqua dolor do amet sint. Velit officia consequat
+            duis enim. Amet minim mollit non deserunt ullamco est sit aliqua
+            dolor do amet sint. Velit officia consequat duis enim. Amet minim
+            mollit non deserunt ullamco est sit aliqua do amet sint. Velit
+            officia consequat duis enim. Amet minim mollit non deserunt ullamco
+            est sit aliqua dolor do amet sint.
+          </Description>
         </Content>
-        <Image src={man} alt="a black man holding a smart phone." />
+        <Image src={doctor} alt="a black man holding a smart phone." />
       </Container>
     </StyledSection>
   );
 };
+
 export default About;
