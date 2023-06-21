45d7d614ce0c0a501120ab53d98ef5d745fbf703
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-20 23:15:30 -0300

    feat: Home page component.

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/sections/Hero";
+import Services from "../components/sections/Services";
+
+const Index = () => {
+  return (
+    <>
+      <Hero />
+      <Services />
+    </>
+  );
+};
+
+export default Index;
