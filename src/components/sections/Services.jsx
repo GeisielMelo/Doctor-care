36ed1ac3919ee09ee7014fc726d7bbfe32b8b158
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-20 23:17:31 -0300

    feat: Created initial state for the Services section.

---
+++ src/components/sections/Services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  padding: 0px 100px;
+  height: 100vh;
+`;
+
+const Services = () => {
+  return <StyledSection></StyledSection>;
+};
+
+export default Services;
