68fba7ced156e4db2731b73f755a68d1a245d8cc
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-22 17:15:06 -0300

    feat: Implemented a fixed navbar that uses useEffect to show only when the mouse scroll is scrolled up, and hide when the scroll is scrolled down.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "../components/sections/Hero";
 import Numbers from "../components/Numbers";
 import Services from "../components/sections/Services";
@@ -8,9 +8,41 @@ import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 
 const Index = () => {
+  const [activeProp, setActiveProp] = useState("-70px");
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos =
+        window.pageYOffset || document.documentElement.scrollTop;
+  
+      if (currentScrollPos > prevScrollPos) {
+        setActiveProp("-70px");
+      } else if (currentScrollPos < prevScrollPos && currentScrollPos > 70) {
+        setActiveProp("0px");
+      } else if (currentScrollPos === 0) {
+        setActiveProp("-70px");
+      }
+  
+      setPrevScrollPos(currentScrollPos);
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+  
   return (
     <>
-      <Nav />
+      <Nav
+        navPos="fixed"
+        navTop={activeProp}
+        backgroundColor="#00856F"
+        textColor="#fff"
+        borderColor="#fff"
+      />
       <Hero />
       <Numbers />
       <Services />
