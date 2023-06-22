397491dc35eee4661f790b692ee3dd9bf5404e39
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-22 17:12:35 -0300

    feat: Created new component Menu.jsx, which is responsible for displaying the mobile menu when called.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
+import Button from "./Button";
+import CloseIcon from "../images/close.png";
+import Media from "./Media";
+import WhatsAppLink from "../utils/WhatsAppLink"
 
-const MenuContainer = styled.div`
+const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  padding: 20px;
 
   position: fixed;
   top: 0;
@@ -16,11 +21,74 @@ const MenuContainer = styled.div`
   z-index: 9999;
 `;
 
-const Menu = () => {
+const Head = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-height: 70px;
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  list-style-type: none;
+  margin-top: 50px;
+  margin-bottom: 20px;
+`;
+
+const Item = styled.li`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 150%;
+  margin: 20px 0;
+  position: relative;
+  cursor: pointer;
+  color: #ffffff;
+  
+  a:visited {
+  color: inherit;
+}
+`;
+
+const CloseButton = styled.button`
+  background: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+const Menu = ({ closeMenu }) => {
   return (
-    <MenuContainer>
-      <Logo />
-    </MenuContainer>
+    <Container>
+      <Head>
+        <Logo colorOne={"#ffffff"} colorTwo={"#ffffff"} />
+        <CloseButton onClick={closeMenu}>
+          <img src={CloseIcon} alt="close menu" />
+        </CloseButton>
+      </Head>
+
+      <List>
+        <Item onClick={closeMenu}><a href="#hero">Inicio</a></Item>
+        <Item onClick={closeMenu}><a href="#about">Sobre</a></Item>
+        <Item onClick={closeMenu}><a href="#services">Serviços</a></Item>
+        <Item onClick={closeMenu}><a href="#contact">Depoimentos</a></Item>
+      </List>
+
+      <Button onClick={WhatsAppLink}
+        style={{
+          border: "1px solid #ffffff",
+          backgroundColor: "#ffffff",
+          color: "#00856f",
+          marginBottom: "20px",
+        }}
+      >
+        Agendar consulta
+      </Button>
+      <Media />
+    </Container>
   );
 };
 export default Menu;
