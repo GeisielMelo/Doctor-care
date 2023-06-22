118685a80293bad14e8d0b4f9415781a7d856a75
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-22 16:56:37 -0300

    feat: Created utils folder with two modules, WhatsAppLinks and MediaLinks.

---
+++ src/utils/MediaLinks.js
@@ -0,0 +1,11 @@
+export const instagramUrl = () => {
+  window.open("https://github.com/GeisielMelo", "_blank");
+};
+
+export const facebookUrl = () => {
+  window.open("https://github.com/GeisielMelo", "_blank");
+};
+
+export const youTubeUrl = () => {
+  window.open("https://github.com/GeisielMelo", "_blank");
+};
---
+++ src/utils/WhatsAppLink.js
@@ -0,0 +1,5 @@
+const WhatsAppLink = () => {
+  window.open("https://github.com/GeisielMelo", "_blank");
+};
+
+export default WhatsAppLink;
