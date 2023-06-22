dd039379a44f1667329b5bb928b6a90c2907e1a6
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-22 17:04:06 -0300

    refactor: the Logo component now has colorOne and colorTwo parameters to define the colors.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,17 +6,19 @@ const StyledLogo = styled.h1`
   font-size: 24px;
   line-height: 1;
   color: #212529;
+  ${(props) => props.colorOne && `color: ${props.colorOne}`}
 `;
 
 const StyledLogoSpan = styled.span`
   font-weight: 400;
   color: #00856f;
+  ${(props) => props.colorTwo && `color: ${props.colorTwo}`}
 `;
 
-const Logo = () => {
+const Logo = ({ colorOne, colorTwo }) => {
   return (
-    <StyledLogo>
-      Doctor<StyledLogoSpan>Care</StyledLogoSpan>
+    <StyledLogo colorOne={colorOne}>
+      Doctor<StyledLogoSpan colorTwo={colorTwo}>Care</StyledLogoSpan>
     </StyledLogo>
   );
 };
